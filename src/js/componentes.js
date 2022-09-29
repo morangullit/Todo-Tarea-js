e4b0f8a0177f24a9f7e62ee90226f7cf1e8a5786
Gullit Moran <gullitmoran@hotmail.com>
2022-09-29 17:42:36 -0500

    WebPack Nuv.

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -6,7 +6,7 @@ export const saludar = ( nombre = 'sin nombre' ) => {
     console.log('Creando etiqueta h1');
 
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${ nombre }`;
+    h1.innerText = `Hola, mi nombre es: ${ nombre }`;
 
     document.body.append( h1 );
 
