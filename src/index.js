25e9c4f6bf775b95ac36f98ca3ed723f6861d5e3
Gullit Moran <gullitmoran@hotmail.com>
2022-09-30 12:31:57 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import './styles.css';
 
 import { Todo, TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
-const todoList = new TodoList();
+export const todoList = new TodoList();
 
-const tarea = new Todo('Aprender Java Script');
-
-todoList.nuevoTodo(tarea);
-
-
-console.log(todoList);
-
+todoList.todos.forEach(todo => crearTodoHtml(todo));
